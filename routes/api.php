6685a08098ed11c1a26fffabb208ad02f37d2fae
Izzan Abdul <izzanabdul123@gmail.com>
2024-08-07 23:27:24 +0700

    feat: fix route api

--- routes/api.php
+++ routes/api.php
@@ -20,19 +20,7 @@ Route::post('/courses/{course_id}/contents', [ContentController::class, 'store']
 Route::put('/courses/{course_id}/contents/{id}', [ContentController::class, 'update']); // UPDATE CONTENT
 Route::delete('/courses/{course_id}/contents/{id}', [ContentController::class, 'delete']); // DELETE CONTENT
 
-Route::get('/courses/popular', [CourseController::class, 'popular']);
-Route::get('/api/courses/enrolled/{user_id}', [CourseController::class, 'purchased']);
-Route::get('/courses/{id}', [CourseController::class, 'detail']);
-Route::post('/courses', [CourseController::class, 'store']);
-Route::put('/courses/{id}', [CourseController::class, 'update']);
-Route::delete('/courses/{id}', [CourseController::class, 'delete']);
+Route::put('/api/topup/{user_id}', [UserController::class, 'topup']); // TOPUP BALANCE
 
-Route::post('/courses/{course_id}/content', [ContentController::class, 'store']);
-Route::put('/courses/{course_id}/content/{id}', [ContentController::class, 'update']);
-Route::delete('/courses/{course_id}/content/{id}', [ContentController::class, 'delete']);
-
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register/{role}', [AuthController::class, 'register']);
-
-Route::put('/api/topup/{user_id}', [UserController::class, 'topup']);
+Route::post('/login', [AuthController::class, 'login']); // LOGIN USER
+Route::post('/register/{role}', [AuthController::class, 'register']); // REGISTER USER & INSTRUCTOR
