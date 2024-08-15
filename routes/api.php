2ce3d1b28212062330c0cba8f12ad813bfc3ab88
Danan <narayanmade578@gmail.com>
2024-08-16 02:11:38 +0700

    fix(routes): change method from PATCH to POST

--- routes/api.php
+++ routes/api.php
@@ -30,47 +30,47 @@ Route::middleware('user')->group(function() {
     Route::get('/courses/filter', [CourseController::class, 'filterCourses']);
     Route::get('/courses/{id}', [CourseController::class, 'detail']); // GET DETAIL COURSE BY ID
     Route::post('/courses', [CourseController::class, 'store']); // ADD NEW COURSE
-    Route::patch('/courses/{id}', [CourseController::class, 'update']); // UPDATE COURSE BY ID
+    Route::post('/courses/{id}', [CourseController::class, 'update']); // UPDATE COURSE BY ID
     Route::delete('/courses/{id}', [CourseController::class, 'delete']); // DELETE COURSE BY ID
-    
+
     // Contents
     Route::get('/courses/{course_id}/contents', [ContentController::class, 'allContents']); // GET ALL CONTENTS DATA
     Route::get('/courses/{course_id}/contents/{id}', [ContentController::class, 'detail']); // GET CONTENT DETAIL BY ID
     Route::post('/courses/{course_id}/contents', [ContentController::class, 'store']); // ADD NEW CONTENT
-    Route::patch('/courses/{course_id}/contents/{id}', [ContentController::class, 'update']); // UPDATE CONTENT
+    Route::post('/courses/{course_id}/contents/{id}', [ContentController::class, 'update']); // UPDATE CONTENT
     Route::delete('/courses/contents/{id}', [ContentController::class, 'delete']); // DELETE CONTENT
-    
+
     // Balance
     Route::post('/topup/{user_id}', [UserController::class, 'topup']); // TOPUP BALANCE
     Route::get('/balance/{user_id}', [UserController::class, 'getBalance']); // GET BALANCE
-    
+
     // Category
     Route::post('/category', [CategoryController::class, 'store']); // ADD NEW CATEGORY
     Route::get('/category', [CategoryController::class, 'getUniqueCategories']); // GET UNIQUE CATEGORIES
-    
+
     // Ratings
     Route::get('/courses/{course_id}/ratings', [RatingController::class, 'getRatingsForCourse']); // GET RATINGS BY COURSE ID
     Route::post('/courses/{course_id}/ratings/{user_id}', [RatingController::class, 'addRatings']); // ADD NEW RATINGS
-    
+
     // Assignments
     Route::get('/courses/{course_id}/assignments', [AssignmentController::class, 'getAssignmentByCourseId']); // GET ASSIGNMENT BY COURSE ID
     Route::get('/assignments/detail/{id}', [AssignmentController::class, 'show']);
     Route::get('/assignments/{instructor_id}', [AssignmentController::class, 'getAssignmentByInstructorId']); // GET ASSIGNMENT BY INSTRUCTOR ID
     Route::get('assignments/deadline/{assignment_id}/{user_id}', [AnswerController::class, 'getAssignmentDeadline']); // GET ASSIGNMENT DEADLINE
     Route::post('/courses/{course_id}/assignments', [AssignmentController::class, 'store']);
-    Route::patch('/courses/{course_id}/assignments/{id}', [AssignmentController::class, 'update']);
+    Route::post('/courses/{course_id}/assignments/{id}', [AssignmentController::class, 'update']);
     Route::delete('/assignments/{id}', [AssignmentController::class, 'delete']);
 
     // Answer
     Route::get('/assignments/{assignment_id}/answers/{user_id}', [AnswerController::class, 'getAnswer']); // GET ANSWER BY ASSIGNMENT ID AND USER ID
     Route::get('/assignments/{assignment_id}/answers', [AnswerController::class, 'getAllAnswers']); // GET ALL ANSWER BY ASSIGNMENT ID
     Route::post('/assignments/{assignment_id}/answers', [AnswerController::class, 'store']); // ADD NEW ANSWER
-    Route::patch('/assignments/{assignment_id/answers/{answer_id}', [AnswerController::class, 'update']); // UPDATE TASK, STATUS, GRADE 
+    Route::patch('/assignments/{assignment_id/answers/{answer_id}', [AnswerController::class, 'update']); // UPDATE TASK, STATUS, GRADE
 
     // Enrollment
     Route::get('enrollment/{course_id}', [EnrollmentController::class, 'getUsersByCourseId']); // GET ALL USERS ENROLLED IN THE COURSE
     Route::post('/enrollment/{course_id}/{user_id}', [EnrollmentController::class, 'store']); // ADD NEW ENROLLMENT
-    
+
     // Progress
     Route::get('/progress/contents/{content_id}/{user_id}', [ProgressController::class, 'checkProgress']); // GET USER PROGRESS IN CONTENT
     Route::get('/progress/courses/{course_id}/{user_id}', [ProgressController::class, 'countProgress']); // GET USER PROGRESS
