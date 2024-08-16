6bc624609bc20aa5443ee0f4dea2ea2c8da6a982
Rayhan Fadhlan <rayhanfazka07@gmail.com>
2024-08-16 21:46:32 +0700

    fix : delete some api route from protected

--- routes/api.php
+++ routes/api.php
@@ -18,38 +18,46 @@ Route::post('/login', [AuthController::class, 'login']); // LOGIN USER
 Route::post('/register/{role}', [AuthController::class, 'register']); // REGISTER USER & INSTRUCTOR
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('user');
 
+
+// Courses
+Route::get('/courses/popular', [CourseController::class, 'popular']); // GET POPULAR COURSES WITH LIMIT 10
+Route::get('/courses/category/{category_id}', [CourseController::class, 'getCoursesByCategoryId']); // GET COURSES BY CATEGORY ID
+Route::get('/courses/level/{level}', [CourseController::class, 'getCoursesByLevel']); // GET COURSES BY DIFFICULTY LEVEL
+Route::get('/courses/ratings', [CourseController::class, 'getCoursesByRatingRange']); // GET COURSES BY RATINGS RANGE
+Route::get('/courses/filter', [CourseController::class, 'filterCourses']);
+Route::get('/courses/{id}', [CourseController::class, 'detail']); // GET DETAIL COURSE BY ID
+
+
+// Category
+Route::get('/category', [CategoryController::class, 'getUniqueCategories']); // GET UNIQUE CATEGORIES
+
+// Ratings
+Route::get('/courses/{course_id}/ratings', [RatingController::class, 'getRatingsForCourse']); // GET RATINGS BY COURSE ID
+
 // User Middleware
 Route::middleware('user')->group(function() {
     // Courses
-    Route::get('/courses/popular', [CourseController::class, 'popular']); // GET POPULAR COURSES WITH LIMIT 10
-    Route::get('/courses/category/{category_id}', [CourseController::class, 'getCoursesByCategoryId']); // GET COURSES BY CATEGORY ID
-    Route::get('/courses/level/{level}', [CourseController::class, 'getCoursesByLevel']); // GET COURSES BY DIFFICULTY LEVEL
-    Route::get('/courses/ratings', [CourseController::class, 'getCoursesByRatingRange']); // GET COURSES BY RATINGS RANGE
     Route::get('/courses/enrolled/{user_id}', [CourseController::class, 'purchased']); // GET
     Route::get('/courses/instructor/{instructor_id}', [CourseController::class, 'getInstructorCourse']); // GET COURSES BY INSTRUCTOR ID
-    Route::get('/courses/filter', [CourseController::class, 'filterCourses']);
-    Route::get('/courses/{id}', [CourseController::class, 'detail']); // GET DETAIL COURSE BY ID
     Route::post('/courses', [CourseController::class, 'store']); // ADD NEW COURSE
     Route::post('/courses/{id}', [CourseController::class, 'update']); // UPDATE COURSE BY ID
     Route::delete('/courses/{id}', [CourseController::class, 'delete']); // DELETE COURSE BY ID
-
+    
     // Contents
     Route::get('/courses/{course_id}/contents', [ContentController::class, 'allContents']); // GET ALL CONTENTS DATA
     Route::get('/courses/{course_id}/contents/{id}', [ContentController::class, 'detail']); // GET CONTENT DETAIL BY ID
     Route::post('/courses/{course_id}/contents', [ContentController::class, 'store']); // ADD NEW CONTENT
     Route::post('/courses/{course_id}/contents/{id}', [ContentController::class, 'update']); // UPDATE CONTENT
     Route::delete('/courses/contents/{id}', [ContentController::class, 'delete']); // DELETE CONTENT
-
+    
     // Balance
     Route::post('/topup/{user_id}', [UserController::class, 'topup']); // TOPUP BALANCE
     Route::get('/balance/{user_id}', [UserController::class, 'getBalance']); // GET BALANCE
-
+    
     // Category
     Route::post('/category', [CategoryController::class, 'store']); // ADD NEW CATEGORY
-    Route::get('/category', [CategoryController::class, 'getUniqueCategories']); // GET UNIQUE CATEGORIES
-
+    
     // Ratings
-    Route::get('/courses/{course_id}/ratings', [RatingController::class, 'getRatingsForCourse']); // GET RATINGS BY COURSE ID
     Route::post('/courses/{course_id}/ratings/{user_id}', [RatingController::class, 'addRatings']); // ADD NEW RATINGS
 
     // Assignments
